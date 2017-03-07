14b8a47eb5703c66df2fbb8145881ecec44767da
Rintarou <merlin.barzilai@gmail.com>
2017-03-07 21:43:16 +0100

    credits manquants

--- script/MenuState.js
+++ script/MenuState.js
@@ -11,11 +11,11 @@ MenuState.prototype.constructor = MenuState;
 MenuState.prototype.create=function() {
     this.bg = game.add.image(0, 0, 'bg_menu');
     
-    this.level1 = game.add.button(game.world.centerX - 95, 200, 'level1', this.startLevel, this, 2, 1, 0);
-    this.level2 = game.add.button(game.world.centerX - 95, 255, 'level2', this.startLevel, this, 2, 1, 0);
-    this.level3 = game.add.button(game.world.centerX - 95, 310, 'level3', this.startLevel, this, 2, 1, 0);
+    this.level1 = game.add.button(game.world.centerX - 95, 200, 'b_level1', this.startLevel, this, 2, 1, 0);
+    this.level2 = game.add.button(game.world.centerX - 95, 255, 'b_level2', this.startLevel, this, 2, 1, 0);
+    this.level3 = game.add.button(game.world.centerX - 95, 310, 'b_level3', this.startLevel, this, 2, 1, 0);
 
-    this.credits = game.add.button(game.world.centerX - 95, 355, 'credits', this.startLevel, this, 2, 1, 0);
+    this.credits = game.add.button(game.world.centerX - 95, 355, 'b_credits', this.credits, this, 2, 1, 0);
 }
 
 MenuState.prototype.update=function() {
@@ -24,4 +24,12 @@ MenuState.prototype.update=function() {
 
 MenuState.prototype.startLevel= function() {
     game.state.start('LevelState',true,false,0);
+}
+
+MenuState.prototype.credits = function() {
+    this.displayedCredits = game.add.button(0, 0, 'bg_menu', this.returnMenu, this, 2, 1, 0);
+}
+
+MenuState.prototype.returnMenu = function() {
+    game.world.remove(this.displayedCredits);
 }
