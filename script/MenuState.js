79b3634fc4ad5c345bd474cf12c9dd3149acb5b4
Biratus <clement.birette@hotmail.Fr>
2017-03-08 13:49:59 +0000

    Add files via upload

--- script/MenuState.js
+++ script/MenuState.js
@@ -10,20 +10,20 @@ MenuState.prototype.constructor = MenuState;
 
 MenuState.prototype.create=function() {
     this.bg = game.add.image(0, 0, 'bg_menu');
+    this.bg.width=game.world.width;
+    this.bg.height=game.world.height;
     
     this.level1 = game.add.button(game.world.centerX - 95, 200, 'b_level1', this.startLevel, this, 2, 1, 0);
+    this.level1.toLevel=0;
     this.level2 = game.add.button(game.world.centerX - 95, 255, 'b_level2', this.startLevel, this, 2, 1, 0);
-    this.level3 = game.add.button(game.world.centerX - 95, 310, 'b_level3', this.startLevel, this, 2, 1, 0);
+    this.level1.toLevel=1;
+    this.level3 = game.add.button(game.world.centerX - 95, 310, 'b_level3', null, this, 2, 1, 0);
 
     this.credits = game.add.button(game.world.centerX - 95, 355, 'b_credits', this.credits, this, 2, 1, 0);
 }
 
-MenuState.prototype.update=function() {
-
-}
-
-MenuState.prototype.startLevel= function() {
-    game.state.start('LevelState',true,false,0);
+MenuState.prototype.startLevel= function(button) {
+    game.state.start('LevelState',true,false,button.toLevel);
 }
 
 MenuState.prototype.credits = function() {
