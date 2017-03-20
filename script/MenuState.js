4b289e43c54932fd0b11bc38c92f239cc4446e33
Rintarou <merlin.barzilai@gmail.com>
2017-03-20 17:39:15 +0100

    prevent overwrite merge

--- script/MenuState.js
+++ script/MenuState.js
@@ -10,17 +10,17 @@ MenuState.prototype.constructor = MenuState;
 
 MenuState.prototype.create=function() {
     //game.state.start('LevelState',true,false,0);
-    // this.bg = game.add.image(0, 0, 'bg_menu');
-    // this.bg.width=game.world.width;
-    // this.bg.height=game.world.height;
-    //
-    // this.level1 = game.add.button(game.world.centerX - 95, 200, 'b_level1', this.startLevel, this, 2, 1, 0);
-    // this.level1.toLevel=0;
-    // this.level2 = game.add.button(game.world.centerX - 95, 255, 'b_level2', this.startLevel, this, 2, 1, 0);
-    // this.level1.toLevel=0;
-    // this.level3 = game.add.button(game.world.centerX - 95, 310, 'b_level3', null, this, 2, 1, 0);
-    //
-    // this.credits = game.add.button(game.world.centerX - 95, 355, 'b_credits', this.credits, this, 2, 1, 0);
+    this.bg = game.add.image(0, 0, 'bg_menu');
+    this.bg.width=game.world.width;
+    this.bg.height=game.world.height;
+
+    this.level1 = game.add.button(game.world.centerX - 95, 200, 'b_level1', this.startLevel, this, 2, 1, 0);
+    this.level1.toLevel=0;
+    this.level2 = game.add.button(game.world.centerX - 95, 255, 'b_level2', this.startLevel, this, 2, 1, 0);
+    this.level1.toLevel=0;
+    this.level3 = game.add.button(game.world.centerX - 95, 310, 'b_level3', null, this, 2, 1, 0);
+
+    this.credits = game.add.button(game.world.centerX - 95, 355, 'b_credits', this.credits, this, 2, 1, 0);
 }
 
 MenuState.prototype.startLevel= function(button) {
